4d9ef0c31df2d0b86694993bf2cff78fcfee597e
julian.lorente <julian.lorente@106-16.info2>
2024-09-23 12:28:08 +0200

    Ejercicio 1 actualizado.

--- src/parte1/Ejer1.java
+++ src/parte1/Ejer1.java
@@ -8,11 +8,14 @@ public class Ejer1 {
 		//Creamos el scanner
 		Scanner sc = new Scanner(System.in);
 		
+		//Declaramos la variable num
+		int num;
+		
 		//Imprimimos por pantalla la pregunta
 		System.out.print("Buenas, introduce un número: ");
 		
 		//Espera a que el usuario introduzca el número
-		int num = sc.nextInt();
+		num = sc.nextInt();
 		
 		//Imprime por pantalla el siguiente texto + el número introducido
 		System.out.println("Has introducido el número " + num);
