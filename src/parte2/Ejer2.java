84ef6853797bc790309df7a0eb65634a4c7faed2
julian.lorente <julian.lorente@106-16.info2>
2024-09-30 12:09:46 +0200

    Ejercicio 2 terminado.

---
+++ src/parte2/Ejer2.java
@@ -0,0 +1,27 @@
+package parte2;
+
+import java.util.*;
+
+public class Ejer2 {
+
+	public static void main(String[] args) {
+		// Scanner
+		Scanner reader = new Scanner(System.in);
+		
+		// Declaración de variables
+		int num;
+		
+		// Primer mensaje con lectura del número introducido
+		System.out.print("Introduce un número: ");
+		num = reader.nextInt();
+		
+		// Operaciones
+		num = num % 7;
+		num = 7 - num;
+		
+		// Mensaje final
+		System.out.println("Tienes que sumarle " + num + " para que sea múltiplo de 7.");
+
+	}
+
+}
