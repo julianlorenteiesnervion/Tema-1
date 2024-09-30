87b679f1438bb9e30b967b6e04da346e65942c4e
julian.lorente <julian.lorente@106-16.info2>
2024-09-30 11:56:43 +0200

    Ejercicio 1 terminado.

--- src/parte2/Ejer1.java
+++ src/parte2/Ejer1.java
@@ -10,15 +10,17 @@ public class Ejer1 {
 		
 		// Declaración de variables
 		double num;
+		double num2;
 		
 		// Mensaje y scanner para solicitar el número
 		System.out.print("Bienvenido, introduce un número con decimales: ");
 		num = reader.nextDouble();
 		
 		// Redondeo del número con decimales
-		num = (int) num;
+		num2 = num - (int) num;
 		
-		System.out.println(num);
+		// Mensaje final para mostrar el número redondeado
+		System.out.print(num2 >= 0.5 ? (int) ++num : (int) num);
 		
 	}
 
